34363423a0ef51ffb849b164581cdf7208d946a0
Volodymyr Zankevych <108954365+Mamont257@users.noreply.github.com>
2023-05-17 00:21:08 +0300

    small fix

--- decorators/validateBody.js
+++ decorators/validateBody.js
@@ -3,6 +3,9 @@ const { HttpError } = require("../helpers");
 const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
+    if (!Object.keys(error._original).length) {
+      next(HttpError(400, "missing fields"));
+    }
     if (error) {
       next(HttpError(400, error.message));
     }
