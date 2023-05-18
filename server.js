9064e2b51890b2b361f90e15e2caaecb482ee5a6
Mamont257 <108954365+Mamont257@users.noreply.github.com>
2023-05-18 23:44:58 +0300

    test dep

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Volodymyr:qwertyv@cluster0.jedymrz.mongodb.net/mu_contacts?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    })
+  )
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
